ab5b28ad618a9b5ea57fdd9c6376525459049c03
Priyanshu-choudhary <broyadi23@gmail.com>
2024-09-18 04:12:57 +0530

    versel routes fixed-5

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/github/callback" element={
-          <div style={{ maxWidth: "50%", display: "flex", gap:"1%" }} className="App bg-white rounded-lg">
-          <FontPage />
-          <div style={{ width: 1 }} className="bg-gray-400 min-h-full"></div>
-          <RightContent youtube={youtube} github={github} />
-        </div>
-        } />
+        <Route path="/github/callback" element={<GitHubLoginComponent />} />
         <Route 
           path="/" 
           element={
